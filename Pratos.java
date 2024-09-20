010ea9f9b382bfcf9351da124cb7cfca73ee3d27
JoyceMiranda21 <163652782+JoyceMiranda21@users.noreply.github.com>
2024-09-20 18:00:19 -0300

    Update Pratos.java

--- Pratos.java
+++ Pratos.java
@@ -1,1 +1,23 @@
+package restaurante;
 
+public class Prato {
+    private String nome;
+    private double preco;
+    private String categoria;
+
+    public Prato() {
+        this.nome = "";
+        this.preco = 0.0;
+        this.categoria = "";
+    }
+
+    public Prato(String nome, double preco, String categoria) {
+        this.nome = nome;
+        if (preco >= 0) {
+            this.preco = preco;
+        } else {
+            System.out.println("Preço não pode ser negativo.");
+            this.preco = 0.0;
+        }
+        this.categoria = categoria;
+    }
