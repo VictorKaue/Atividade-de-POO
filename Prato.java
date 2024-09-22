5e1f607a072741b16ce315cd4877c1700ab7e96c
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2024-09-22 20:57:59 -0300

    Update and rename Pratos.java to Prato.java

---
+++ Prato.java
@@ -0,0 +1,59 @@
+package restaurante;
+
+public class Prato {
+	private String nome;
+	private double preco;
+	private String categoria;
+
+	public Prato() {
+		this.nome = "";
+		this.preco = 0.0;
+		this.categoria = "";
+	}
+
+	public Prato(String nome, double preco, String categoria) {
+		this.nome = nome;
+		if (preco >= 0) {
+			this.preco = preco;
+		} else {
+			System.out.println("Preço não pode ser negativo.");
+			this.preco = 0.0;
+		}
+		this.categoria = categoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setPreco(double preco) {
+		if (preco >= 0) {
+			this.preco = preco;
+		} else {
+			System.out.println("Preço não pode ser negativo.");
+			this.preco = 0.0;
+		}
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public void exibirPratos() {
+		System.out.println("Nome: "+ nome);
+		System.out.printf("Preço: %.2f\n", preco);
+		System.out.println("Categoria: " + categoria);
+	}
+}
